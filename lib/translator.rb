fcecced2b59db0211dd7206979d9b453cc25bcfd
Erik Zhao <erikzhao98@gmail.com>
2020-08-01 02:28:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -33,8 +33,7 @@ def get_japanese_emoticon(path, emoticon)
     if emoticon == emoticon_hash[feeling][:english]
       # binding.pry
       return emoticon_hash[feeling][:japanese]
-    else emoticon == nil
-      return "Sorry, that emoticon was not found"
     end
   end
+  return "Sorry, that emoticon was not found"
 end
